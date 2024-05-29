a7e69550e61dbed4cdb179e97ea9adfd8766241a
Михаил Никитин <mihailnicitin@gmail.com>
2024-05-29 22:45:52 +0300

    add dataset

--- generation/generator.py
+++ generation/generator.py
@@ -12,12 +12,22 @@ def generate_dataset(
 ):
     points = [[(i // (size ** (dimension - 1 - j))) % size for j in range(dimension)] for i in range(size ** dimension)]
     weights = [uniform(weights_lb, weights_ub) for _ in range(dimension)]
-    values = [sum([point[i] * weights[i] * uniform(0.9, 1.1) for i in range(dimension)]) for point in points]
+    values = [sum([point[i] * weights[i] * uniform(0.5, 1.5) for i in range(dimension)]) for point in points]
     return points, values
 
 
-if __name__ == '__main__':
-    # points, values = generate_dataset(1, 100, 0.5, 2)
-    # print(json.dumps({"points": points, "values": values}))
+def generate_to_file(
+dimension: int,
+        size: int,
+        weights_lb: float,
+        weights_ub: float,
+        filename: str,
+):
+    points, values = generate_dataset(dimension, size, weights_lb, weights_ub)
+    path_prefix = "./data/"
+    with open(path_prefix + filename, 'w') as f:
+        f.write(json.dumps({"points": points, "values": values}))
 
-    print(load_static_file("dataset_1.json"))
+
+if __name__ == '__main__':
+    generate_to_file(2, 100, 0.5, 10, "noised_data.json")
